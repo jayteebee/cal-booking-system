5af67d49695e62d4cafefc7f3a5f11fdb6133d8d
Jethro Block <jethroblock22@gmail.com>
2025-03-14 10:46:20 +0000

    Refactor routing in App.js to use Routes instead of Switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookingView from './components/BookingView';
 import AdminDashboard from './components/AdminDashboard';
 
 function App() {
+  console.log('App.js');
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/" component={BookingView} />
         <Route path="/admin" component={AdminDashboard} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }

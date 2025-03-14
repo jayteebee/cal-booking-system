bd051c693d315081a9287280ed85148edaea55e2
Jethro Block <jethroblock22@gmail.com>
2025-03-14 10:52:30 +0000

    Add initial TypeScript definitions and configuration files for various modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookingView from './components/BookingView';
 import AdminDashboard from './components/AdminDashboard';
 
 function App() {
-  console.log('App.js');
   return (
     <Router>
       <Routes>
-        <Route exact path="/" component={BookingView} />
-        <Route path="/admin" component={AdminDashboard} />
+        <Route path="/" element={<BookingView />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
